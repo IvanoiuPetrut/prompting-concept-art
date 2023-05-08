abaf67c3127d7126006f66b1b68e3d1797261d6a
Petrut <ivanoiu.petrut27@yahoo.com>
2023-05-08 19:25:29 +0300

    feat: add template for entering a image

---
+++ src/components/GetImage.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="text-5xl font-semibold text-center mb-20">
+    Choose a way to start <strong class="text-primary">creating</strong>
+  </h1>
+  <div class="flex flex-col w-1/2 mx-auto border-opacity-50 lg:flex-row">
+    <div class="grid h-20 card rounded-box place-items-center">
+      <div class="flex gap-2">
+        <input
+          type="text"
+          placeholder="Enter a prompt"
+          class="input input-bordered input-primary w-full max-w-xs"
+        />
+        <button class="btn btn-primary">Enter</button>
+      </div>
+    </div>
+    <div class="divider lg:divider-horizontal">OR</div>
+    <div class="grid h-20 card rounded-box place-items-center">
+      <input
+        type="file"
+        class="file-input file-input-bordered file-input-primary w-full max-w-xs"
+      />
+    </div>
+  </div>
+</template>
--- src/views/CreateImageView.vue
+++ src/views/CreateImageView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import ImageCanvas from "../components/ImageCanvas.vue";
+import GetImage from "@/components/GetImage.vue";
 </script>
 
 <template>
-  <ImageCanvas />
+  <div class="mt-32">
+    <GetImage />
+    <!-- <ImageCanvas /> -->
+  </div>
 </template>
